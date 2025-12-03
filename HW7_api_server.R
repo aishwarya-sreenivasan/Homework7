af2b8d12919c133a3b6446659b7d5ce7188cf12a
aishwarya-sreenivasan <aishwaryas865@gmail.com>
2025-12-02 21:34:54 -0600

    HW files

--- HW7_api_server.R
+++ HW7_api_server.R
@@ -1,1 +1,64 @@
 ## Write your API endpoints in this file
+
+library(plumber2)
+library(tidyverse)
+
+# Load and train model
+training <- read_csv("train_dataset.csv.gz")
+test <- read_csv("test_dataset.csv.gz")
+
+# Calculates the number of days between when the appointment was scheduled and 
+# when it occurs
+
+training$days_between <- as.numeric(difftime(training$appt_time, training$appt_made, units = "days"))
+test$days_between <- as.numeric(difftime(test$appt_time, test$appt_made, units = "days"))
+
+# Encoding categorical variables
+
+training <- training %>%
+  mutate(
+    provider_id = as.factor(provider_id),
+    specialty = as.factor(specialty),
+    address = as.factor(address),
+    no_show = as.factor(no_show)
+  )
+
+test <- test %>%
+  mutate(
+    provider_id = as.factor(provider_id),
+    specialty = as.factor(specialty),
+    address = as.factor(address)
+  )
+
+# Logistic regression to predict probability a patient does not show up to the 
+# appointment, trained on training data set
+
+model <- glm(
+  no_show ~ age + provider_id + address + specialty + days_between,
+  data = training,
+  family = binomial(link = "logit")
+)
+
+# API endpoints
+
+#* Predict probability of no-show
+#* @post /predict_prob
+#* @serializer json
+function(body) {
+  body <- as.data.frame(body)
+  pred <- predict(model, newdata = body, type = "response")
+  as.numeric(pred)
+}
+
+#* Predict class of no-show
+#* @post /predict_class
+#* @serializer json
+function(body) {
+  body <- as.data.frame(body)
+  prob <- predict(model, newdata = body, type = "response")
+  as.numeric(ifelse(prob > 0.5, 1, 0))
+}
+
+# Run server in console:
+## pa <- api("HW7_api_server.R", port = 8080)
+## api_run(pa)
--- HW7_client_test.R
+++ HW7_client_test.R
@@ -1,1 +1,46 @@
 ## Write your client test code here
+
+library(httr)
+library(jsonlite)
+library(tidyverse)
+
+# Load test data
+test <- read_csv("test_dataset.csv.gz")
+
+# Calculates the number of days between when the appointment was scheduled and 
+# when it occurs
+
+test$days_between <- as.numeric(difftime(test$appt_time, test$appt_made, units = "days"))
+
+# Encoding categorical variables
+
+test <- test %>%
+  mutate(
+    provider_id = as.factor(provider_id),
+    specialty = as.factor(specialty),
+    address = as.factor(address)
+  )
+
+# Select predictors
+
+predictors <- test %>%
+  select(age, provider_id, address, specialty, days_between)
+
+# Test /predict_prob
+
+predpoints <- toJSON(predictors)
+r <- POST("http://127.0.0.1:8080/predict_prob", 
+          body = predpoints, 
+          content_type_json())
+result_prob <- content(r, "text") |> fromJSON()
+cat("Probability predictions:\n")
+print(result_prob)
+
+# Test /predict_class
+
+r <- POST("http://127.0.0.1:8080/predict_class", 
+          body = predpoints, 
+          content_type_json())
+result_class <- content(r, "text") |> fromJSON()
+cat("\nClass predictions:\n")
+print(result_class)
